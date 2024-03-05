55feda85c21ab31da583a02b4eb121bf297c5811
FurkanAkinalp <100966886+FurkanAkinalp@users.noreply.github.com>
2024-03-05 10:22:31 +0000

    Create format_kullanım.py

---
+++ format_kullanım.py
@@ -0,0 +1,7 @@
+x = int(input("Lütfen ilk sayıyı giriniz:"))
+y = int(input("Lütfen ikinci sayıyı giriniz:"))
+z = int(input("Lütfen üçüncü sayıyı giriniz:"))
+
+
+
+print("{} x {} x {} = {}".format(x,y,z,(x*y*z)))
