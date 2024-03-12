9198d12b39796343d2051c08e63e0752d29a8c2a
FurkanAkinalp <100966886+FurkanAkinalp@users.noreply.github.com>
2024-03-12 14:43:29 +0000

    Add files via upload

---
+++ 30_Days_Python_Exercise/Day_1/2.py
@@ -0,0 +1,10 @@
+
+x=3
+y=4
+print("{}+{}={}".format(x,y,x+y))
+print("{}-{}={}".format(x,y,x-y))
+print("{}*{}={}".format(x,y,x*y))
+print("{}/{}={}".format(x,y,x/y))
+print("{}%{}={}".format(x,y,x%y))
+print("{}**{}={}".format(x,y,x**y))
+print("{}//{}={}".format(x,y,x//y))
---
+++ 30_Days_Python_Exercise/Day_1/3-4.py
@@ -0,0 +1,10 @@
+name="lehhguman"
+country="Turkiye"
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type(name))
+print(type(country))
